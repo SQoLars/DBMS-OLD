006782e59c381b92e153ca9c1acda8150382a51e
VladLiviuAlexandru <liviu.alexandru.vlad@gmail.com>
2022-11-05 01:47:48 +0200

    Added base files

---
+++ Main/main.cpp
@@ -0,0 +1,26 @@
+//
+//		SQoLars: DBMS
+//		-> A light sqlite-like database management system.
+//
+//		Project realised by students:
+//			Simion Cristina Florentina
+//			Toma Andreea
+//			Vlad Liviu Alexandru
+//			Vlad Rares
+//		- ASE CSIE I.E. Eng G 1068
+//
+//		Repository:
+//		https://github.com/SQoLars/DBMS
+//
+//		Documentation:
+//		https://github.com/SQoLars/DBMS#readme
+//
+
+int main(
+	int argc,
+	char** argv[]
+) {
+
+	return 0;
+}
+
