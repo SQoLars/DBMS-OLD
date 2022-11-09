4dbd9241b892e35396ef5dd8feb5e9b277dba27f
VladLiviuAlexandru <liviu.alexandru.vlad@gmail.com>
2022-11-10 00:44:20 +0200

    Rewrote files class. Created input arguments handling function. Cleaned up redundant code in QOL.h

--- Main/libraries/QOL.h
+++ Main/libraries/QOL.h
@@ -5,15 +5,3 @@ namespace consts {
 	const std::string path = "./";
 	const std::string unk = "unknown";
 }
-
-namespace args {
-	bool arguments = false;
-
-	void setArgs(int argc) {
-		if (argc > 1) arguments = true;
-	}
-
-	bool args() {
-		return arguments;
-	}
-}
--- Main/main.cpp
+++ Main/main.cpp
@@ -18,24 +18,22 @@
 //		Sections: (ctrl+f)
 //			INCLUDE BLOCK
 //			MAIN BLOCK
-
+//          TESTING BLOCK
 
 //=============== INCLUDE BLOCK ===============
 #include "libraries/files/files.h"
 #include "libraries/parse/misc_parse.h"
 #include <iostream>
-#include <typeinfo>
-#include <vector>
 
 //=============== MAIN BLOCK ===============
 int main(
 	int argc,
-	char* argv[]
-) {
-	args::setArgs(argc);
-	std::string current_exec_name = argv[0]; // Name of the current exec program
-	std::vector<std::string> all_args;
-	all_args.assign(argv, argv + argc);
-	
-}
+	char *argv[])
+{
+	Files::File* commands = Args::setArgs(argc, argv);
 
+	//=============== TESTING BLOCK ===============	
+	for (int i(0); i < argc-1; i++)
+		std::cout << i << ": Name: " << commands[i].getName() << " Status: " << commands[i].getStatus() << "\n";
+
+}
